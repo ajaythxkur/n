2fdc068fc276b3fabeb3b1ab67b4a837955b4b11
ajaythxkur <ajaythakur9131@gmail.com>
2022-09-09 17:16:11 +0530

    co

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.use(express.json())
 
 const PORT = 4001;
 
+//Routing
 app.get('/', async (req,res)=>{
+    //get data
     let data ={
         username:'testing',
         email:'test@mail.com',
         password:'test@123'
     }
+    //validation first
     let rules ={
         username:'required|min:5|max:12',
         email:'required|email',
@@ -35,7 +38,7 @@ app.get('/', async (req,res)=>{
             result = {status_code:0, status_text:'failed', message:'Exists'};
         } else{
             //hash before sending
-            data.password = await bcrypt.hash(data.password, 10);
+            data.password = await bcrypt.hashSync(data.password, 10);
             result = await  UserModel.create(data);
         }
     }else{
@@ -46,11 +49,43 @@ app.get('/', async (req,res)=>{
     res.send({status_code:1, status_text:'success', message:'working',result:result});
 })
 
-app.get('/login', (res,req) => {
+app.get('/login', async(req,res) => {
+
     let data ={
-        email: 'testing@mail.com',
-        password: ''
+        email: 'test@mail.com',
+        password: 'test@123'
     }
+
+    let rules = {
+        email: 'required|email',
+        password: 'required'
+    }
+
+    let validation = new Validator(data,rules);
+    // data.password = await bcrypt.compareSync(data.password, UserModel.password);
+    var result;
+    if(validation.passes()){
+        let exist = await UserModel.findAll({
+            where:{
+                email: data.email,
+            }
+        }) 
+        // console.log(exist)
+        if(exist.length>0){
+            const validPassword = await bcrypt.compareSync('$2b$10$SnzBTqU4vPIY/1FeRR3bPu4ndW3ykQAdaC7ibaFVcqlhC1J4pFDru',data.password)
+            if(validPassword){
+                result = {message: 'Password does not match'}
+            }else{
+                result = {message: 'Authorized'}
+            }
+        }else{
+            result = {message: 'Unauthorized'}
+        }
+    }else{
+        result = validation.errors;
+    }
+
+    res.send({status_code:1, status_text:'success', message:'working', result: result});
 })
 
 app.listen(PORT, () => {
